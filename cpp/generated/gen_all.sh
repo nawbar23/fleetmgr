3d4d23c64e5fd4d8a5b576c968d88abe7110c2f3
Bartosz Nawrot <nawbar23@gmail.com>
2018-12-01 23:44:52 +0100

    Temporarly add generated files

---
+++ cpp/generated/gen_all.sh
@@ -0,0 +1,14 @@
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/add_channels.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/attach_channels.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/channel.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/channel_validation.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/connection_state.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/handover_data.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/location.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/common/role.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/core/attach.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/core/list_devices.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/core/operate.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/facade/control/facade_service.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/facade/control/heartbeat.proto
+protoc -I ../../proto --grpc_out=. --cpp_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ../../proto/facade/control/setup.proto
