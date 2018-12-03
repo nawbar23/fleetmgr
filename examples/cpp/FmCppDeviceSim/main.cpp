0c7f8b8918dfbc682175cbd9b235875c710112c0
Bartosz Nawrot <nawbar23@gmail.com>
2018-12-03 19:29:02 +0100

    Add Boost and OpenSSL libs

--- examples/cpp/FmCppDeviceSim/main.cpp
+++ examples/cpp/FmCppDeviceSim/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
-#include "Device.hpp"
-
 #include "example/AsioListener.hpp"
 #include "example/AsioHttpsClient.hpp"
+
+#include "Device.hpp"
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
 int main(int argc, char** argv)
 {
     const std::string coreAddress = "https://localhost:1234";
     const std::string apiKey = "ApiKey asdasdasdasd";
     const std::string certPath = "c:/asd/asd/asd.ca";
+
+    boost::asio::io_service io_service;
 
     AsioListener listener;
     AsioHttpsClient coreClient(coreAddress, apiKey, certPath);
