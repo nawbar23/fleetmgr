66d438a49b5d8d1901af2f1ab0c8d03eff6f9abd
Bartosz Nawrot <nawbar23@gmail.com>
2018-12-08 22:38:40 +0100

    Revert trace removal

--- java/src/main/java/com/fleetmgr/sdk/client/Client.java
+++ java/src/main/java/com/fleetmgr/sdk/client/Client.java
@@ -49,4 +49,9 @@ public abstract class Client extends StateMachine<Event> {
 
         this.backend = new ClientBackend(this::trace, this, listener, executor, coreClient);
     }
+
+    @Override
+    public void trace(String message) {
+        listener.trace(message);
+    }
 }
