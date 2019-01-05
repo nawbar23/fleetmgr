36d3b682d12a33207f650d6169497c2718613f1f
Bartosz Nawrot <nawbar23@gmail.com>
2019-01-05 11:30:59 +0100

    Add handling of incoming handover

--- cpp/source/event/output/HandoverAccepted.cpp
+++ cpp/source/event/output/HandoverAccepted.cpp
@@ -5,7 +5,7 @@ using namespace fm::event::output;
 
 HandoverAccepted::HandoverAccepted(const std::string& _data) :
     FacadeEvent(HANDOVER_ACCEPTED),
-    data()
+    data(_data.begin(), _data.end())
 {
 }
 
@@ -16,5 +16,5 @@ const std::vector<uint8_t>& HandoverAccepted::getData() const
 
 std::string HandoverAccepted::toString() const
 {
-    return "HANDOVER_ACCEPTED: data size: " + data.size();
+    return "HANDOVER_ACCEPTED: data size: " + std::to_string(data.size());
 }
--- cpp/source/state/pilot/connected/ReleasingControl.cpp
+++ cpp/source/state/pilot/connected/ReleasingControl.cpp
@@ -1,6 +1,16 @@
 #include "state/pilot/connected/ReleasingControl.hpp"
 
+#include "state/pilot/connected/Operating.hpp"
+
+#include "event/output/ReleaseControl.hpp"
+
+#include "event/input/user/ReleaseAccepted.hpp"
+#include "event/input/user/ReleaseRejected.hpp"
+
 #include "event/input/connection/Received.hpp"
+
+#include "backend/ClientBackend.hpp"
+#include "backend/ChannelsHandler.hpp"
 
 using namespace fm;
 using namespace fm::state;
@@ -22,6 +32,7 @@ ReleasingControl::ReleasingControl(IState& state, long _channelId) :
 
 IState::State ReleasingControl::start()
 {
+    listener.onEvent(std::make_shared<ReleaseControl>(channelId));
     return nullptr;
 }
 
@@ -29,6 +40,29 @@ IState::State ReleasingControl::handleUserEvent(const UserEvent& event)
 {
     switch (event.getType())
     {
+    case UserEvent::RELEASE_ACCEPTED:
+    {
+        const ReleaseAccepted releaseEvent = reinterpret_cast<const ReleaseAccepted&>(event);
+        ClientMessage clientMessage;
+        clientMessage.set_command(Command::RELEASE_CONTROL);
+        clientMessage.set_response(Response::ACCEPTED);
+        clientMessage.mutable_handoverdata()->set_handoverdata(
+                    std::string(releaseEvent.getData().begin(), releaseEvent.getData().end()));
+        send(clientMessage);
+        return nullptr;
+    }
+
+    case UserEvent::RELEASE_REJECTED:
+    {
+        const ReleaseRejected releaseEvent = reinterpret_cast<const ReleaseRejected&>(event);
+        ClientMessage clientMessage;
+        clientMessage.set_command(Command::RELEASE_CONTROL);
+        clientMessage.set_response(Response::ACCEPTED);
+        clientMessage.set_message(releaseEvent.getMessage());
+        send(clientMessage);
+        return std::make_unique<Operating>(*this);
+    }
+
     default:
         return defaultEventHandle(event.toString());
     }
@@ -38,6 +72,9 @@ IState::State ReleasingControl::handleConnectionEvent(const ConnectionEvent& eve
 {
     switch (event.getType())
     {
+    case ConnectionEvent::RECEIVED:
+        return handleMessage(reinterpret_cast<const Received&>(event).getMessage());
+
     default:
         return defaultEventHandle(event.toString());
     }
@@ -52,6 +89,17 @@ IState::State ReleasingControl::handleMessage(const ControlMessage& message)
 {
     switch (message.command())
     {
+    case Command::CONTROL_RELEASED:
+    {
+        backend.getChannelsHandler().setOwned(channelId, false);
+        listener.onEvent(std::make_shared<FacadeEvent>(FacadeEvent::CONTROL_RELEASED));
+        ClientMessage clientMessage;
+        clientMessage.set_command(Command::CONTROL_RELEASED);
+        clientMessage.set_response(Response::ACCEPTED);
+        send(clientMessage);
+        return std::make_unique<Operating>(*this);
+    }
+
     default:
         return defaultMessageHandle(message);
     }
